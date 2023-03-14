94c888a3d02804aa678a621c01efcf12bc1c215c
Kuanysh <kuanish2203@gmail.com>
2023-03-15 01:47:08 +0600

    was added CRUD opeation

--- kdStydy/base/urls.py
+++ kdStydy/base/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+
+    path('create-room/',views.createRoom, name="create-Room"),
+    path('update-room/<str:pk>/',views.updateRoom, name="update-Room"),
+    path('delete-room/<str:pk>/',views.deleteRoom, name="delete-Room")
 ]
  
