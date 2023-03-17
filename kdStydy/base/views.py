8d83fb02a2c8138feefd3abe7b51485be1cbb26f
Kuanysh <kuanish2203@gmail.com>
2023-03-17 23:06:11 +0600

    was added redirected page functionality

--- kdStydy/base/views.py
+++ kdStydy/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render ,redirect
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from .models import Room , Topic
+from django.http import HttpResponse
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate , login , logout
@@ -62,6 +64,7 @@ def room (request,pk):
     context = {'room': room }
     return  render(request,'base/room.html',context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm
     if request.method == 'POST':
@@ -74,9 +77,14 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('your not allowed here')
+
 
     if request.method == 'POST':
         form = RoomForm(request.POST ,instance=room)
@@ -87,6 +95,8 @@ def updateRoom(request, pk):
     context= {'form': form}
     return render(request, 'base/room_form.html',context)
 
+
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     
